413a6def8b41f042ffe17f74e3a2a1305da32f44
Dhruva Sharma <Dhruva1098@gmail.com>
2024-05-15 20:49:25 +0530

    added type casting type:const_cast

---
+++ type casting/const_cast.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class A {
+	int a_;
+public:
+	A(int a) : a_(a) {};
+	int get() const { return a_; }
+	void set(int a) { a_ = a; }
+};
+void print(char* str) { std::cout << str << std::endl; }
+
+int main() {
+	const char* str = "hello world";
+	//print(str);     this will give an error as it expects a char* but we provide a const char*
+	print(const_cast<char*>(str)); //this will however work as const_cast casts 
+
+	const A a(1);
+	a.get();
+	
+	// a.set(6)      throws an error because set cannot convert a this pointer of const A to A
+	const_cast<A&>(a).set(6); //works
+	// const_cast<A>(a).set(6);     also throws an error because const_cast cannot convert const A to A
+}
