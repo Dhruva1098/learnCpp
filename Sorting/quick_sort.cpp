0d5a23a6f3760e451ce86eea610ac537422f3248
Dhruva Sharma <dhruva1098@gmail.com>
2024-07-22 22:01:24 +0530

    added quick sort

--- Sorting/quick_sort.cpp
+++ Sorting/quick_sort.cpp
@@ -1,5 +1,21 @@
 #include <vector>
 
+void partition(std::vector<int>& array, int low, int high) {
+	int pivot = array[low];
+	int i = low;
+	ing j = highl
+	while (i < j) {
+		while (array[i] <= pivot && i <= high - 1) {
+			i++;
+		}
+		while (array[j] > pivot && j >= low + 1) {
+			j--;
+		}
+		if (i < j) swap(array[i], array[j]);
+	}
+	swap(array[low], array[j]);
+	return j;
+}
 void quick_sort(std::vector<int>& array, int low, int high) {
 	if (low < high) {
 		int pIndex = partition(arr, low, high);
