d815ef93c038ca2a2753627ce36f7484b518b60e
Dhruva Sharma <dhruva1098@gmail.com>
2024-08-03 14:29:58 +0530

    hashmap

--- dsa/hashmap.cpp
+++ dsa/hashmap.cpp
@@ -1,10 +1,55 @@
-struct node {
-    int data;
-    node* next;
+#include <iostream>
+#include <algorithm>
+const int ksize = 10000;
+const int kprime = 100006;
+class node {
+  int data;
+  node* next;
+public:
+  node(int data) : data(data) , next(nullptr) {}
+
+  void insert(int data, node*& head) {
+    node* temp = new node(data);
+    temp->next = head;
+    head = temp;
+  }
+
+  bool find_val(int data, node*& head) {
+    node* current = head;
+    while (current->next != nullptr) {
+      if (current->data == data) {
+        return true;
+      }
+      current = current->next;
+    } return false;
+  }
+
+  void delete_val(int data, node*& head) {
+    if(head->data == data) {
+      node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+    node* current = head;
+    while (current->next != nullptr && current->next->data != data) {
+      current = current->next;
+    };
+    if (current->next != nullptr) {
+      node* temp = current->next;
+      current->next = current->next->next;
+      delete temp;
+    }
+  }
 };
-typedef struct node node;
+
+static node* kArray[ksize];
+std::fill(kArray, kArray + ksize, nullptr);
 
 class Hashmap {
-    node* map[2000];
+  node **map = kArray;
 public:
+  void insert(int in) {
+    int pos = (in % kprime) % ksize;
+    insert(in, *map[pos]);       
+  }
 };
