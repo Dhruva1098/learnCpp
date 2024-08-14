47978a608118f6e61046a66ceca247588652a97f
Dhruva Sharma <dhruva1098@gmail.com>
2024-08-14 19:47:50 +0530

    largest index to right

--- assignments/Algorithm/PracticeProblems_1/18.cpp
+++ assignments/Algorithm/PracticeProblems_1/18.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <stack>
 #include <vector>
-std::vector<int> smallest_index_to_right(std::vector<int> array){
+std::vector<int> largest_index_to_right(std::vector<int> array){
     int n = array.size();
-    std::vector<int> out(n,n);
+    std::vector<int> out(n,-1);
     std::stack<int> stack;
 
     for(int i = 0; i < n ; i++){
-        while(!stack.empty() && array[stack.top()] >= array[i]) {
+        while(!stack.empty() && array[stack.top()] <= array[i]) {
             out[stack.top()] = i;
            stack.pop();
         }
@@ -17,7 +17,7 @@ std::vector<int> smallest_index_to_right(std::vector<int> array){
 }
 int main(){
     std::vector<int> array = {4,5,7,23,1,6,8,0,2};
-    std::vector<int> out = smallest_index_to_right(array);
+    std::vector<int> out = largest_index_to_right(array);
     for(int i : out){
         std::cout << i << " ";
     };
