f8b328f2c106abd57e5d4d1eb5adb2b4dcbd9e9b
Dhruva Sharma <dhruva1098@gmail.com>
2024-09-07 10:56:53 +0530

    LC104

---
+++ dsa/LC_104.cpp
@@ -0,0 +1,15 @@
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
+  TreeNode(int value, TreeNode* leftp, TreeNode* rightp) : val(value), left(leftp), right(rightp) {}
+};
+int maxDepth(TreeNode* root){
+  if(root == nullptr) return 0;
+  int lt = maxDepth(root->left);
+  int rt = maxDepth(root->right);
+  return 1 + lt>=rt?lt:rt;
+}
+
