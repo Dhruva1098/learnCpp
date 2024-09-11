621f9ac9dc8701c5e65f32a4e3f135c4df86328b
Dhruva Sharma <dhruva1098@gmail.com>
2024-09-11 18:17:50 +0530

    gfg_bottom_view_bTree

---
+++ dsa/bottom_view_bTree.cpp
@@ -0,0 +1,27 @@
+#include <map>
+#include <utility>
+#include <queue>
+using namespace std;
+struct TreeNode{
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+};
+vector<int> topView(TreeNode* root){
+  vector<int> out;
+  if(root == NULL) return out;
+  map<int,int> map;
+  queue<pair<TreeNode*,int>> q;
+  q.push({root,0});
+  while(!q.empty()){
+    auto p = q.front();
+    TreeNode* node = p.first;
+    int line = p.second;
+    map[line] = node->val;
+    if(node->left)q.push({node->left,line-1});
+    if(node->right)q.push({node->right,line+1});
+  }
+  for(auto p: map){
+    out.push_back(p.second);
+  } return out; 
+}
