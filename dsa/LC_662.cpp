7ada94915784cbb07849941bc397123451bfb0e7
Dhruva Sharma <dhruva1098@gmail.com>
2024-09-14 10:23:22 +0530

    LC_662

---
+++ dsa/LC_662.cpp
@@ -0,0 +1,33 @@
+#include <utility>
+#include <queue>
+#include <iostream>
+typedef unsigned long long ull;
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+};
+int max(int a, int b){
+  return a>=b?a:b;
+}
+int maxWidth(TreeNode* root){
+  if(root==NULL) return 0;
+  int ans = 0;
+  std::queue<std::pair<TreeNode*, ull>> q;
+  q.push({root,0});
+  while(!q.empty()){
+    ull mmin = q.front().second;
+    int size = q.size();
+    ull first; ull last;
+    for(int i = 0; i < size; i++){
+      TreeNode* node = q.front().first;
+      ull curr = q.front().second - mmin;
+      q.pop();
+      if(i == 0) first = curr;
+      if( i == size - 1) last = curr;
+      if(node->left)q.push({node->left, curr*2 + 1});
+      if(node->right)q.push({node->right, curr*2 + 2});
+    } ans = max(ans, (int)(last - first - 1));
+  } return ans;
+}
