f258d085683855c3354d2e7f57895772a3ccd95d
Dhruva Sharma <dhruva1098@gmail.com>
2024-10-31 23:39:59 +0530

    1991A

--- dsa/1991A.cpp
+++ dsa/1991A.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
-#include <
+#include <queue>
+using namespace std;
+int main(){
+  int n;
+  cin >> n;
+  long long rsum = 0;
+  priority_queue<int> pq;
+  for(int i = 0; i<n; i++){
+    int j;
+    cin >> j;
+    if(j<0){
+      pq.push(j);
+    }
+    if(rsum + j > 0) rsum += j;
+    else if(rsum)
+  }
+}
