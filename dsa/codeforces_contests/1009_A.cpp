a3d1343713b11624ff7d02dc773ebec74928917e
Dhruva Sharma <dhruva1098@gmail.com>
2025-03-11 20:41:02 +0530

    1009 A

---
+++ dsa/codeforces_contests/1009_A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int l, r, d, u;
+        cin >> l >> r >> d >> u;
+        if(l == r && d == u && l == d) cout << "Yes\n";
+        else cout << "No\n";
+    }
+}
