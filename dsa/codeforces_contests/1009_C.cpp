59feaf842db38c921ecb574371e71c0a10511c99
Dhruva Sharma <dhruva1098@gmail.com>
2025-03-12 22:59:10 +0530

    some more A questions, will move on to B

--- dsa/codeforces_contests/1009_C.cpp
+++ dsa/codeforces_contests/1009_C.cpp
@@ -1,5 +1,4 @@
 #include  <iostream>
-// Remove the <bit> header as it's C++20 specific
 
 using namespace std;
 int main(){
@@ -13,13 +12,25 @@ int main(){
             cout << -1 << "\n";
             continue;
         }
-        for(int i = 0; i < 30; i++){
-            for(int j = 0; j < 30; j++){
-                int y = (1 << i) | (1 << j);
-                if(y < x && ((x+y) > x^y) && y + (x^y) > x){
-                    ans = y;
+        int z = 0;
+        int f1 = 0, f0 = 0;
+        for(int i = 0; i <31 ; i++){
+            if(x & (1 << i)){
+                if(f0 && f1 == 0){
+                    z |= 1 << i;
+                    f1 = 1;
+                }
+            } else {
+                if(f0 == 0){
+                    z |= 1 << i;
+                    f0 = 1;
                 }
             }
-        } cout << ans << "\n";
-    }
+        }
+        if (f1) {
+            int y = x ^ z;
+            cout << y << "\n";
+        } else 
+            cout << -1 << "\n";
+        }
 }
