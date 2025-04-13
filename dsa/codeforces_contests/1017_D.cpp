1a4182809e3cef597edd72ab776a7f8867ace389
Dhruva Sharma <dhruva1098@gmail.com>
2025-04-13 23:02:02 +0530

    codeforces round 1017 more

--- dsa/codeforces_contests/1017_D.cpp
+++ dsa/codeforces_contests/1017_D.cpp
@@ -1,31 +1,68 @@
 #include <iostream>
 #include <string>
-int arr[200000];
-int arrp[200000];
+#include <cstring>
+int arr[200005];
+int arrp[200005];
+char arr_char[200005];
+char arrp_char[200005];
 using namespace std;
 int main(){
+  ios_base::sync_with_stdio(false);  cin.tie(NULL);
+
   int t;
   cin >> t;
   while(t--){
+    memset(arr, 0, sizeof(arr));
+    memset(arrp, 0, sizeof(arrp));
     string p,s;
     cin >> p >> s;
-    if(p[0] != s[0]){ cout << "no" << "\n"; continue;} 
+    if(p[0] != s[0]){ cout << "NO" << "\n"; continue;}
+
     int pl = p.length();
     int sl = s.length();
-    int j = 0; int count = 1;
+
+    int j_p = 0; int count = 1;
     for(int i = 1; i < pl; i++){
-      if(p[i] == p[i-1]) count ++;
-      if(p[i] != p[i-1]){ arr[j++] = count; count = 1;}
-    } arr[j] = count; count = 1; j = 0;
-    for(int i = 1; i < sl; i++){
-      if(s[i] == s[i-1]) count ++;
-      if(s[i] != s[i-1]){ arrp[j++] = count; count = 1;}
-    } arrp[j] = count;
-    int k = 0; bool flag = true;
-    while(arr[k]){
-      if(2*arr[k] < arrp[k] || arr[k] > arrp[k] || p[arr[k] - 1] != s[arrp[k] - 1]) flag = false;
-      k++;
+      if(p[i] == p[i-1]) {
+          count ++;
+      } else {
+          arr[j_p] = count;
+          arr_char[j_p] = p[i-1];
+          j_p++;
+          count = 1;
+      }
     }
-    if(flag) cout << "yes\n"; else cout << "no\n";
+    arr[j_p] = count;
+    arr_char[j_p] = p[pl-1];
+    j_p++; 
+
+    int j_s = 0; count = 1;
+    for(int i = 1; i < sl; i++){
+      if(s[i] == s[i-1]) {
+          count ++;
+      } else {
+          arrp[j_s] = count;
+          arrp_char[j_s] = s[i-1];
+          j_s++;
+          count = 1;
+      }
+    }
+    arrp[j_s] = count;
+    arrp_char[j_s] = s[sl-1];
+    j_s++;
+
+    bool flag = true;
+    if(j_p != j_s) {
+        flag = false;
+    } else {
+        for(int k = 0; k < j_p; k++) {
+        if(arr_char[k] != arrp_char[k] || arr[k] > arrp[k] || arrp[k] > 2 * arr[k]) {
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    if(flag) cout << "YES\n"; else cout << "NO\n";
   }
 }
